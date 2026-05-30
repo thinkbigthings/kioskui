deedec437d651ce8cde1b7ade86c1608e386f27a
Jason Young <thinkbigthings@gmail.com>
2026-05-30 06:05:20 -0400

    assign led color dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import type { LockerNumbered } from './types'
+
+interface KioskState {
+  lockers: LockerNumbered[]
+}
 
 function App() {
+  const [lockers, setLockers] = useState<LockerNumbered[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/sim/kiosk')
+      .then((res) => res.json())
+      .then((data: KioskState) => setLockers(data.lockers))
+      .catch((err) => console.error('Failed to load kiosk state', err))
+  }, [])
+
+  const led = lockers[0]?.locker.led
+
   return (
     <div className="kiosk">
       <div className="locker">
-        <div className="led" />
+        <div
+          className="led"
+          style={
+            led
+              ? { background: `rgb(${led.red}, ${led.green}, ${led.blue})` }
+              : undefined
+          }
+        />
       </div>
     </div>
   )
